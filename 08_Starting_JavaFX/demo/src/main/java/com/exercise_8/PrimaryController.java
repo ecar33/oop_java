b7e05d7aa2f3c5b731004940e4c395650cf7ae3e
Evan Carlile <evanc2022@gmail.com>
2024-03-06 13:14:43 -0600

    Finished assignent 8

--- 08_Starting_JavaFX/demo/src/main/java/com/exercise_8/PrimaryController.java
+++ 08_Starting_JavaFX/demo/src/main/java/com/exercise_8/PrimaryController.java
@@ -1,12 +1,33 @@
 package com.exercise_8;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TextField nameField;
+
+    @FXML
+    private TextField numberField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextArea outputArea;
+
+    @FXML
+    private void handleSubmitButtonAction() {
+        String name = nameField.getText();
+        String number = numberField.getText();
+        String email = emailField.getText();
+        
+        // Format the output
+        String output = String.format("Name: %s\nNumber: %s\nEmail: %s", name, number, email);
+        
+        // Set the output
+        outputArea.setText(output);
     }
 }
